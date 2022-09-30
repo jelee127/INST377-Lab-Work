1a89691ccdd5455a48c63c16d0b179616ae6c90c
Jaeeun Lee <chsezbet@gmail.com>
2022-09-30 00:04:07 -0400

    Update script.js

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -14,31 +14,31 @@ document.addEventListener('DOMContentLoaded',() => {
       [width, width*2, width*2+1, width*2+2]
   ]
   const zTetromino = [
-      [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
-      [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1],
-      [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
-      [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1]
+      [0, width, width + 1, width * 2 + 1],
+      [width + 1, width + 2, width * 2, width * 2 + 1],
+      [0, width, width + 1, width * 2 + 1],
+      [width + 1, width + 2, width * 2, width * 2 + 1]
     ]
 
   const tTetromino = [
-      [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
-      [1, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
-      [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
-      [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1]
+      [1, width, width + 1, width + 2],
+      [1, width + 1, width + 2, width * 2 + 1],
+      [width, width + 1, width + 2, width * 2 + 1],
+      [1, width, width + 1, width * 2 + 1]
     ]
 
   const oTetromino = [
-      [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
-      [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
-      [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
-      [0, 1, GRID_WIDTH, GRID_WIDTH + 1]
+      [0, 1, width, width + 1],
+      [0, 1, width, width + 1],
+      [0, 1, width, width + 1],
+      [0, 1, width, width + 1]
     ]
 
   const iTetromino = [
-      [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
-      [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3],
-      [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
-      [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3]
+      [1, width + 1, width * 2 + 1, width * 3 + 1],
+      [width, width + 1, width + 2, width + 3],
+      [1, width + 1, width * 2 + 1, width * 3 + 1],
+      [width, width + 1, width + 2, width + 3]
   ]
 
   const theTetrominoes = [lTetrominom, zTetromino, tTetromino, oTetromino, iTetromino]
