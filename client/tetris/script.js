eb40ddc960dd0681673241b0690b07ca1c0dae9e
Jaeeun Lee <chsezbet@gmail.com>
2022-09-29 23:58:12 -0400

    Update script.js

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -4,54 +4,54 @@ document.addEventListener('DOMContentLoaded',() => {
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
   const width = 10;
-})
 
 //Tetrominoes
 
-const lTetromino = [
-    [1, width+1, width*2+1, 2],
-    [width, width+1, width+2, width*2+2],
-    [1, width+1, width*2+1, width*2],
-    [width, width*2, width*2+1, width*2+2]
-]
-const zTetromino = [
-    [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
-    [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1],
-    [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
-    [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1]
+  const lTetromino = [
+      [1, width+1, width*2+1, 2],
+      [width, width+1, width+2, width*2+2],
+      [1, width+1, width*2+1, width*2],
+      [width, width*2, width*2+1, width*2+2]
+  ]
+  const zTetromino = [
+      [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+      [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1],
+      [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+      [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1]
+    ]
+
+  const tTetromino = [
+      [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
+      [1, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+      [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+      [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1]
+    ]
+
+  const oTetromino = [
+      [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+      [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+      [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+      [0, 1, GRID_WIDTH, GRID_WIDTH + 1]
+    ]
+
+  const iTetromino = [
+      [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
+      [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3],
+      [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
+      [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3]
   ]
 
-const tTetromino = [
-    [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
-    [1, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
-    [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
-    [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1]
-  ]
+  const theTetrominoes = [lTetrominom, zTetromino, tTetromino, oTetromino, iTetromino]
 
-const oTetromino = [
-    [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
-    [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
-    [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
-    [0, 1, GRID_WIDTH, GRID_WIDTH + 1]
-  ]
+  let currentPosition = 4
+  let current = theTetrominoes[0][0]
 
-const iTetromino = [
-    [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
-    [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3],
-    [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
-    [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3]
-]
+  //draw the first rotation
+  function draw(){
+      current.forEach(index => {
+          squares[currentPosition+index].classList.add('tetromino')
+      })
+  }
 
-const theTetrominoes = [lTetrominom, zTetromino, tTetromino, oTetromino, iTetromino]
-
-let currentPosition = 4
-let current = theTetrominoes[0][0]
-
-//draw the first rotation
-function draw(){
-    current.forEach(index => {
-        squares[currentPosition+index].classList.add('tetromino')
-    })
-}
-
-draw()
+  draw()
+})
