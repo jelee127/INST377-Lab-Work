2fc434c69e3c6867c6569f3dd598245fdb979749
Jaeeun Lee <chsezbet@gmail.com>
2022-10-26 23:16:22 -0400

    currentList

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -114,8 +114,11 @@ function getRandomIntInclusive(min, max){
       loadAnimation.classList.remove('lds-ellipsis');
       loadAnimation.classList.add('lds-ellipsis_hidden');
 
+      let currentList = [] 
+
       form.addEventListener('input', (event) => {
           console.log(event.target.value);
+          injectHTML(currentList);
       })
 
       // And here's an eventListener! It's listening for a "submit" button specifically being clicked
@@ -125,11 +128,11 @@ function getRandomIntInclusive(min, max){
         submitEvent.preventDefault();
   
         // This constant will have the value of your 15-restaurant collection when it processes
-        const restaurantList = processRestaurants(arrayFromJson.data);
-        console.log(restaurantList);
+        currentList = processRestaurants(arrayFromJson.data);
+       
   
         // And this function call will perform the "side effect" of injecting the HTML list for you
-        injectHTML(restaurantList);
+        injectHTML(currentList);
         
         // By separating the functions, we open the possibility of regenerating the list
         // without having to retrieve fresh data every time
